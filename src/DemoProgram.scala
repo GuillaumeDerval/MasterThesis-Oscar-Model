58e7d53acbd9ec3bb43e1a8d0ef8ca7d1088f605
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2015-10-20 11:25:13 +0200

    SEND+MORE=MONEY

--- src/DemoProgram.scala
+++ src/DemoProgram.scala
@@ -6,40 +6,24 @@ import algebra.BoolExpression._
 import visualisation.ConstraintsVisualisation
 
 class DemoDistributedModel extends ModelDeclaration with CPSearch {
-  val x1 = IntVar(2, 3)
-  val x2 = IntVar(20, 30)
+  val s = IntVar(0, 9, Some("S"))
+  val e = IntVar(0, 9, Some("E"))
+  val n = IntVar(0, 9, Some("N"))
+  val d = IntVar(0, 9, Some("D"))
+  val m = IntVar(0, 9, Some("M"))
+  val o = IntVar(0, 9, Some("O"))
+  val r = IntVar(0, 9, Some("R"))
+  val y = IntVar(0, 9, Some("Y"))
 
-  x2.updateMax(25)
-
-  val xx = Array.tabulate(5)(x => IntVar(x, 2*x))
-  val xy = Array.tabulate(5)(x => x)
-
-  val t1 = x2 == 25 //min=0, max=1
-  val t2 = t1 + 2 //min=2, max=3
-  val t3 = xx(x1) //min=2, max=6
-  val t4 = t2 ~** t3 //min=4, max=729
-  val t5 = xy(x1) //min=2, max=3
-  val t6 = t4 - t5 //min=1, max=727
-  val c = ((x2 == 25) + 2) ~** xx(x1) - xy(x1) //should be the same a t6
-
-  val varFromC = c.reify()
-
-  post(x2 == 25)
-  post(c) //implicit conversion to c != 0
+  // constraints
+  val c = s*1000 + e*100 + n*10 + d +
+    m*1000 + o*100 + r*10 + e ==
+    m*10000 + o*1000 + n*100 + e*10 + y
+  post(c)
+  //add(allDifferent(all), Strong)
 
   setSearch {
-    println("x1 "+x1.min+" "+x1.max)
-    println("x2 "+x2.min+" "+x2.max)
-    println("t1 "+t1.min+" "+t1.max)
-    println("t2 "+t2.min+" "+t2.max)
-    println("t3 "+t3.min+" "+t3.max)
-    println("t4 "+t4.min+" "+t4.max)
-    println("t5 "+t5.min+" "+t5.max)
-    println("t6 "+t6.min+" "+t6.max)
-    println("c "+c.min+" "+c.max)
-    println("varFromC "+varFromC.min+" "+varFromC.max)
-
-    new ConstraintsVisualisation(Array[Constraint](c == 1)).display()
+    new ConstraintsVisualisation(Array[Constraint](c)).display()
   }
 
   onSolution {}
