3d6eb610c3500d3e722af5bd8241c04d70bd8e05
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2016-04-11 23:46:27 +0200

    Make UnionFindStorage more immutable

--- src/main/scala/misc/UnionFindStorage.scala
+++ src/main/scala/misc/UnionFindStorage.scala
@@ -20,14 +20,21 @@ object UnionFindStorage {
     })
     new UnionFindStorage[StoredType](new_array)
   }
+
+  /**
+    * Copy a UnionFindStorage
+    * @param to_copy UnionFindStorage to be copied
+    */
+  def apply[StoredType](to_copy: UnionFindStorage[StoredType]) = {
+    //Everything that is inside the array is immutable, so we just have to copy the array
+    new UnionFindStorage[StoredType](to_copy.array.clone())
+  }
 }
 
 /**
- * A (semi-mutable union-find implementation that does objet-storage (each set have one object)
- * The union operation is NOT commutative!
- * Constraint: the number of entries can only increase
+ * A (semi-mutable) union-find implementation that does object-storage (each set have one object)
+ * Constraint: the number of entry can only increase
  */
-//TODO: should provide a thread-safe implem, this is not the case for now
 class UnionFindStorage[StoredType](private val array: ArrayBuffer[Elem[StoredType]]) extends Serializable {
   /**
    * Add a new element to the union-find, creating a new set
@@ -41,25 +48,16 @@ class UnionFindStorage[StoredType](private val array: ArrayBuffer[Elem[StoredTyp
   }
 
   /**
-   * Add a new element to the union-find, adding it to another set
-   * @param as_set_member
-   * @return
+   * Do a union. It will drop the value of dropthis and dropThisToo, and point it to useThisInstead.
+   * @param dropThis first element to be "merged"
+   * @param dropThisToo second element to be "merged"
+   * @param useThisInstead element that is the merge result of dropThis and dropThisToo
    */
-  def add(as_set_member: Int): Int = {
-    val newid = array.length
-    array.append(new ElemLink(newid, as_set_member))
-    newid
-  }
-
-  /**
-   * Do a union. It will drop the value of dropthis, and point it to usethis.
-   * @param dropthis
-   * @param usethis
-   */
-  def union(dropthis: Int, usethis: Int) = {
-    val rd = array(dropthis).get(array).pos
-    val ru = array(usethis).get(array).pos
-    array(rd) = new ElemLink(rd, ru)
+  def union(dropThis: Int, dropThisToo: Int, useThisInstead: StoredType) = {
+    val pos1 = array(dropThis   ).get(array).pos
+    val pos2 = array(dropThisToo).get(array).pos
+    array(pos1) = new ElemMain[StoredType](pos1, useThisInstead)
+    array(pos2) = new ElemLink(pos2, pos1)
   }
 
   /**
@@ -88,18 +86,18 @@ private abstract class Elem[StoredType](val pos: Int) extends Serializable {
   def get(array: ArrayBuffer[Elem[StoredType]]): ElemMain[StoredType]
 }
 
-private class ElemMain[StoredType](pos: Int, private var elem: StoredType) extends Elem[StoredType](pos) {
+private class ElemMain[StoredType](pos: Int, elem: StoredType) extends Elem[StoredType](pos) {
   def get(array: ArrayBuffer[Elem[StoredType]]): ElemMain[StoredType] = this
 
   def getContent: StoredType = elem
-
-  def setContent(newval: StoredType) = elem = newval
 }
 
-private class ElemLink[StoredType](pos: Int, private var to: Int) extends Elem[StoredType](pos) {
+private class ElemLink[StoredType](pos: Int, to: Int) extends Elem[StoredType](pos) {
   def get(array: ArrayBuffer[Elem[StoredType]]): ElemMain[StoredType] = {
     val top = array(to).get(array)
-    to = top.pos
+    //Replace ourselve if we are not up-to-date
+    if(top.pos != to)
+      array(pos) = new ElemLink[StoredType](pos, top.pos)
     top
   }
 }
